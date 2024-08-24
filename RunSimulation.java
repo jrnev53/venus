edfcedddbf39ec12bc996b7674ba73cb0e62297d
John Neville <jrnev53@gmail.com>
2024-08-23 20:09:12 -0400

    Create RunSimulation.java

---
+++ RunSimulation.java
@@ -0,0 +1,19 @@
+class RunSimulation
+{
+    public static void main(String[] args) {
+        RunSimulation rs = new RunSimulation();
+        rs.load();
+        rs.run();
+    }
+
+    private void load() {
+    }
+
+    RunSimulation() {
+
+    }
+
+    private void run() {
+        
+    }
+}
